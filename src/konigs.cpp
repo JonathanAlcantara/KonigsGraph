d2cb569de58658e4e8c60a9d6da9f8d22c7ca56b
Gabriel Xara <gabrielxara@gmail.com>
2019-09-01 16:21:52 -0300

    fixed konigs.cpp

--- src/konigs.cpp
+++ src/konigs.cpp
@@ -1,22 +1,2 @@
 #include "konigs.h"
 
-VERTEX_NUM KonigsGraph::KonigsGraph() {
-}
-
-KonigsGraph::~KonigsGraph(5) {
-}
-
-void KonigsGraph::loadGraph(bool matrixAndVector = false) 
-{            
-    ifstream graph;
-    string fileName; 
-    cout << "Por favor, digite o nome do arquivo que contém o grafo" << endl;
-    getline(cin, fileName);
-    
-    graph.open(fileName);
-    
-    if (!graph) {
-        cout << "Arquivo " << path << " Nao foi encontrado.";
-        return;
-    }
-}
--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,4 @@ int main(){
 
     file_handler.openFile("/home/gxara/UFRJ/Teoria dos Grafos/Trabalhos/KonigsGraph/grafo.txt");
     
-    // KonigsGraph::konigs();
 }
