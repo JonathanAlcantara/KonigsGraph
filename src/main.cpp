a47c96824aa8494104db0858a6dac090a92922e1
Gabriel Xara <gabrielxara@gmail.com>
2019-09-01 16:26:21 -0300

    Start implementation of konigs loadGraph

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,10 @@ int main(){
     FileHandler file_handler;
 
     file_handler.openFile("/home/gxara/UFRJ/Teoria dos Grafos/Trabalhos/KonigsGraph/grafo.txt");
+
+    bool matrix = true;
+    std::cout << "Please choos how you want the graph to be handled" << std::endl;
+    std::cout << " [1] Matrix of adjacences" << std::endl;
+    std::cout << " [2] Array of adjacences" << std::endl;
     
 }
