bfdc0e2a7adb465a66db27db8b72f85d84b46ed0
Jonathan Alcantara <jonathan.silva@corp.globo.com>
2019-09-01 23:59:27 -0300

    Remove code that isn't necessary anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,5 @@
 #include "konigs.h"
-#include "fileHandler.h"
-#include <iostream>
 
 int main(){
     std::cout << "Hello world, Konigs!" << std::endl;
-    
-    FileHandler file_handler;
-
-    file_handler.openFile("/home/gxara/UFRJ/Teoria dos Grafos/Trabalhos/KonigsGraph/grafo.txt");
-    
-    int vertex_qty = file_handler.get_vertex_qty();
-    KonigsGraph konigs(vertex_qty);
 }
