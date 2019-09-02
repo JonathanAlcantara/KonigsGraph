5f864ff647a412cc5c15e468e2a2be3872dfc3a2
Jonathan Alcantara <jonathan.silva@corp.globo.com>
2019-09-01 23:54:35 -0300

    Update Class Constructor

--- src/konigs.cpp
+++ src/konigs.cpp
@@ -1,42 +1,6 @@
 #include "konigs.h"
-#include <iostream>
 
-KonigsGraph::KonigsGraph(int vertex_qty) {
-    VERTEX_NUM = vertex_qty;
-    std::cout << "Please choose how you want the graph to be handled" << std::endl;
-    std::cout << " [1] Matrix of adjacences" << std::endl;
-    std::cout << " [2] Array of adjacences" << std::endl;    
-    int answer;
-    std::cin >> answer;
-    if (answer == 1) {
-        loadGraph(false);
-    } else if (answer == 2) {
-        loadGraph(true);
-    } else {
-        std::cout << " Invalid answer!" << std::endl;
-    }
-
-};
-
-KonigsGraph::~KonigsGraph() {
-    
-};
-
-void KonigsGraph::loadGraph(bool matrixAndVecor = false) {
-    if (!matrixAndVecor) { // Implements matrix and initializes it with false for every edge
-        adjMatrix = new bool*[VERTEX_NUM + 1];
-
-        for (int i = 0; i < VERTEX_NUM + 1; ++i) {
-            adjMatrix[i] = new bool[VERTEX_NUM];
-        }
-
-        for (int i = 0; i <= VERTEX_NUM; ++i) {
-            for (int j = 0; j <= VERTEX_NUM; ++j) {
-                adjMatrix[i][j] = false;
-            }
-        }
-        
-    } else {
-        // Implements array
-    }
-};
+KonigsGraph::KonigsGraph(std::string file){
+    KonigsGraph::loadGraphFromFile(file);
+    KonigsGraph::stats();
+}
