175e73b71402d84d36509ab62fb356aeb5d9ffe5
Gabriel Xara <gabrielxara@gmail.com>
2019-09-03 11:21:59 -0300

    Testing BFS and DFS

--- src/main.cpp
+++ src/main.cpp
@@ -5,4 +5,15 @@ int main(){
 
     KonigsGraph konigs("/home/gxara/UFRJ/Teoria dos Grafos/Trabalhos/KonigsGraph/grafo.txt");
     konigs.printStats();
+
+    std::tuple<std::vector<unsigned>, std::vector<unsigned>> x =  konigs.DFS(2);
+    
+    std::vector<unsigned> dadVector =  std::get<0>(x);
+    std::vector<unsigned> heightVector =  std::get<1>(x);
+
+    for (unsigned i = 0; i <= dadVector.size() ; i++) {
+        std::cout << "Altura do vértice" << i << ": " << heightVector[i] << std::endl;
+        std::cout << "Pai do vértice" << i << ": " << dadVector[i] << std::endl;
+
+    }
 }
