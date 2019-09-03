42c292f8d476e900b6f26a4abd568d71da7079d1
Jonathan Alcantara <jonathan.silva@corp.globo.com>
2019-09-03 08:51:47 -0300

    Fix to attend requirements

--- src/konigs.cpp
+++ src/konigs.cpp
@@ -12,18 +12,7 @@ void KonigsGraph::loadGraphFromFile(std::string file, bool createMatrix = true, 
 
     std::string line;
     std::getline(inFile, line);
-    unsigned numOfLines = std::stoi(line);
-    unsigned numberOfNodes;
-    for (unsigned i = 0; i < numOfLines; i++){
-        std::getline(inFile, line);
-        std::istringstream iss(line);
-        unsigned a, b;
-        if (!(iss >> a >> b)) { break; } // error
-        numberOfNodes = std::max(std::max(numberOfNodes, b), a);
-    }
-    inFile.clear();
-    inFile.seekg(0, std::ios::beg);
-
+    unsigned numberOfNodes = std::stoi(line);
     if(createMatrix){
         //std::vector<std::vector<bool>> tempMatrix (numberOfNodes, std::vector<bool>(numberOfNodes));
         adjMatrix = std::vector<std::vector<bool>> (numberOfNodes, std::vector<bool>(numberOfNodes));
@@ -32,9 +21,8 @@ void KonigsGraph::loadGraphFromFile(std::string file, bool createMatrix = true, 
         adjVector = std::vector<std::vector<unsigned>> (numberOfNodes);
     }
 
-    std::getline(inFile, line);
-    for (unsigned i = 0; i < numOfLines; i++){
-        std::getline(inFile, line);
+    while (std::getline(inFile, line))
+    {
         std::istringstream iss(line);
         unsigned vertex1, vertex2;
         if (!(iss >> vertex1 >> vertex2)) { break; } // error
